e7386ae47176fbf20d8952cd19516fa9adb10a90
TiagoDiass <tiago.costadiasss@gmail.com>
2021-04-26 19:52:07 -0300

    adding validation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(routes)
+app.use(routes);
+app.use(errors());
 
 /**
  * Rota / Recurso
@@ -14,7 +16,7 @@ app.use(routes)
 
 /**
  * Métodos HTTP:
- * 
+ *
  * GET: Buscar/listar uma informação do back-end
  * POST: Criar uma informação no back-end
  * PUT: Alterar uma informação no back-end
@@ -23,15 +25,14 @@ app.use(routes)
 
 /**
  * Tipos de parâmetros:
- * 
+ *
  * Query Params: Parâmetros nomeados enviados na rota após o '?' e
  * geralmente eles servem pra filtros, paginação
- * 
+ *
  * Route Params: Parâmetros utilizados para identificar recursos
- * 
+ *
  * Request Body: Corpo da requisição utilizado para criar ou alterar recursos
  */
-
 
 /**
  * Sobre o BD
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -12,16 +13,56 @@ routes.post('/sessions', SessionController.create);
 
 //Listagem das ongs, método list no insomnia
 routes.get('/ongs', OngController.index);
+
 //Cadastro de ong
-routes.post('/ongs', OngController.create);
+routes.post(
+  '/ongs',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required().min(10).max(11),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2),
+    }),
+  }),
+  OngController.create
+);
 
 //Listar casos de uma determinada ONG
-routes.get('/profile', ProfileController.index);
+routes.get(
+  '/profile',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  ProfileController.index
+);
 
 //Listagem de caso/incident
-routes.get('/incidents', IncidentController.index);
+routes.get(
+  '/incidents',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+    }),
+  }),
+  IncidentController.index
+);
+
 //Cadastro de caso/incident
 routes.post('/incidents', IncidentController.create);
+
 //Exclusão de um caso/incident
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.delete(
+  '/incidents/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  IncidentController.delete
+);
+
 module.exports = routes;
